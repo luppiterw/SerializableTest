f96cd932e1f59cbf51dae8e0d20f026ba87588e2
hughie <530125717@qq.com>
2017-07-03 17:24:33 +0800

    Add a test

--- src/main/java/proxy/ObjectHandler.java
+++ src/main/java/proxy/ObjectHandler.java
@@ -1,5 +1,6 @@
 package main.java.proxy;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
@@ -7,16 +8,21 @@ import java.lang.reflect.Proxy;
  * Created by hughie on 17-7-3.
  * A
  */
-public class ObjectHandler implements InvocationHandlerIml{
-    @Override
-    public Object invoke(Object proxy, Method method) throws Exception {
-        return null;
-    }
+public class ObjectHandler implements InvocationHandler{
+
 
     public Object bind(Object delegate) throws Exception {
         this.delegate = delegate;
-        return null;//Proxy.newProxyInstance(SubjectIml.class, this);
+        return Proxy.newProxyInstance(SubjectIml.class.getClassLoader(), new Class[]{SubjectIml.class},this);
     }
 
     private Object delegate;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("ObjectHandler.bind newProxyInstance-invoke start");
+        Object retObj = method.invoke(delegate);//proxy, args);
+        System.out.println("ObjectHandler.bind newProxyInstance-invoke end");
+        return retObj;
+    }
 }
