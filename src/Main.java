610a264caa7f09199a63f735ca63cc7453fd5c85
hughie <hughie.w@qq.com>
2017-07-03 15:07:52 +0800

    Update test

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,15 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.lang.reflect.Method;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World! Current Encoding = " + System.getProperty("file.encoding"));
-
+        Method a;
+        a.invoke()
 
 //        SingletonTest writeSingletonTest = SingletonTest.INSTANCE;
 //        Tester.SpaceSerial.saveSingletonTest(writeSingletonTest);
